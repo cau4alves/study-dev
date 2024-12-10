2694ca48ba1a51e930ec0481b192ce3212d36def
cau4alves <cau4alves@gmail.com>
2024-12-10 17:09:45 -0300

    added javascript fundamentals

---
+++ src/js/fundamental.js
@@ -0,0 +1,27 @@
+// 
+let variavel = 'sou alteravel'
+const constante = 'não é redeclaravel'
+
+variavel = 1
+//proibido reassinar uma constante
+// constante = 2
+
+//console
+console.log(variavel)
+console.error(constante)
+console.info('sou uma informação')
+console.warn('var é depreciado')
+
+// tipos de dados 
+const lastName = 'Bezerra'
+let age = 18
+const person = {
+    name: 'Cauã',
+    age: 18,
+    gender: 'male'
+}
+
+const fruits = ['banana', 'uva']
+
+// console.table(fruits)
+
